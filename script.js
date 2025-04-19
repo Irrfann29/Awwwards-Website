2a47f6ce50a7b7619f2f58d6974e0dc164dd84c5
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-04-19 07:16:10 +0530

    Added loading animation

---
+++ script.js
@@ -0,0 +1,37 @@
+var videoContainer = document.querySelector(".videoCont");
+var playbtn = document.querySelector(".play");
+
+videoContainer.addEventListener("mouseenter", function() {
+    gsap.to(playbtn,{
+        opacity:1,
+        scale :1,
+    })
+})
+videoContainer.addEventListener("mouseleave", function() {
+    gsap.to(playbtn,{
+        opacity:0,
+        scale :0,
+    })
+})
+videoContainer.addEventListener("mousemove", function(dets) {
+    gsap.to(playbtn,{
+        duration : 0.5,
+        delay :0.1,
+        left: dets.clientX - 30,
+        top : dets.clientY -30,
+    })
+})
+gsap.from(".page1 h1",{
+    y: 100,
+    delay:0.4,
+    duration : 0.7,
+    stagger : 0.2,
+    opacity : 0.2,
+})
+gsap.from(".videoCont",{
+    y: 100,
+    delay : 1,
+    duration : 0.3,
+    stagger : 0.2,
+    opacity : 0,
+})
